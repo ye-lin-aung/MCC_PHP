a979ab7c6f0cdb0c14de4a2ccfe4b9b5ee0a85c5
Ye lin aung <hello.yelinaung@gmail.com>
2016-08-29 00:05:15 +0630

    Added Security Rules that make user able to skip urls

--- reg.php
+++ reg.php
@@ -1,18 +1,38 @@
 <?php
-	include  'student.php';
-	require_once 'auth.php';
-	require_once 'databasehelper.php';
+include  'student.php';
+require_once 'auth.php';
+require_once 'databasehelper.php';
+
+session_start();
+
+$required = array('name', 'password');
+
+// Loop over field names, make sure each one exists and is not empty
+$error = false;
+foreach($required as $field) {
+	if (empty($_POST[$field])) {
+		$error = true;
+	}
+}
+
+if ($error) {
+	header("Location:register.php");
+} else {
 	
-	session_start();
+
+
 
 	$student = new Student;
-    $student->createWithPost($_POST,0);
+	$student->createWithPost($_POST,0);
+
 	if(!isUserAlreadyExists($student)){
-    registerUser($student);
-    echo "Hello from php : ".$student->skill;
+		registerUser($student);
+		echo "Hello from php : ".$student->skill;
 	}else{
-	$_SESSION['errors'] = array("User already exists");
-	header("Location:register.php");
+		$_SESSION['errors'] = array("User already exists");
+		header("Location:register.php");
 	}
+}
+
     //Disconnect Database;
 ?>
